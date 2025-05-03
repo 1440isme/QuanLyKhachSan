f7bbe0f987fde46dfbb077c25ef5f1b93c8d4d76
1440isme <23110184@student.hcmute.edu.vn>
2025-05-03 17:34:41 +0700

    ui nganhang

--- KhachSan/frmThongTinNganHang.Designer.cs
+++ KhachSan/frmThongTinNganHang.Designer.cs
@@ -60,8 +60,8 @@
             this.btnThoat});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 3, 0);
-            this.toolStrip1.Size = new System.Drawing.Size(943, 66);
+            this.toolStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 2, 0);
+            this.toolStrip1.Size = new System.Drawing.Size(629, 54);
             this.toolStrip1.TabIndex = 2;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -70,7 +70,7 @@
             this.btnLuu.Image = global::KhachSan.Properties.Resources._285657_floppy_guardar_save_icon;
             this.btnLuu.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.btnLuu.Name = "btnLuu";
-            this.btnLuu.Size = new System.Drawing.Size(45, 61);
+            this.btnLuu.Size = new System.Drawing.Size(36, 51);
             this.btnLuu.Text = "Lưu";
             this.btnLuu.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
@@ -80,7 +80,7 @@
             this.btnThoat.Image = global::KhachSan.Properties.Resources._5235241;
             this.btnThoat.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.btnThoat.Name = "btnThoat";
-            this.btnThoat.Size = new System.Drawing.Size(61, 61);
+            this.btnThoat.Size = new System.Drawing.Size(42, 51);
             this.btnThoat.Text = "Thoát";
             this.btnThoat.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
@@ -90,45 +90,50 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Red;
-            this.label4.Location = new System.Drawing.Point(270, 56);
+            this.label4.Location = new System.Drawing.Point(180, 38);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(413, 39);
+            this.label4.Size = new System.Drawing.Size(286, 27);
             this.label4.TabIndex = 9;
             this.label4.Text = "Thông Tin Chuyển Khoản";
             // 
             // txt_NoiDung
             // 
             this.txt_NoiDung.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_NoiDung.Location = new System.Drawing.Point(323, 390);
+            this.txt_NoiDung.Location = new System.Drawing.Point(215, 267);
+            this.txt_NoiDung.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_NoiDung.Name = "txt_NoiDung";
-            this.txt_NoiDung.Size = new System.Drawing.Size(380, 32);
+            this.txt_NoiDung.Size = new System.Drawing.Size(255, 24);
             this.txt_NoiDung.TabIndex = 15;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(198, 393);
+            this.label3.Location = new System.Drawing.Point(132, 269);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(116, 24);
+            this.label3.Size = new System.Drawing.Size(81, 17);
             this.label3.TabIndex = 14;
             this.label3.Text = "Nội Dung :";
             // 
             // txt_STK
             // 
             this.txt_STK.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_STK.Location = new System.Drawing.Point(322, 290);
+            this.txt_STK.Location = new System.Drawing.Point(215, 198);
+            this.txt_STK.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_STK.Name = "txt_STK";
-            this.txt_STK.Size = new System.Drawing.Size(380, 32);
+            this.txt_STK.Size = new System.Drawing.Size(255, 24);
             this.txt_STK.TabIndex = 13;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(181, 241);
+            this.label1.Location = new System.Drawing.Point(121, 165);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(135, 24);
+            this.label1.Size = new System.Drawing.Size(95, 17);
             this.label1.TabIndex = 11;
             this.label1.Text = "Ngân Hàng :";
             // 
@@ -136,9 +141,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(162, 290);
+            this.label2.Location = new System.Drawing.Point(108, 198);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(154, 24);
+            this.label2.Size = new System.Drawing.Size(108, 17);
             this.label2.TabIndex = 10;
             this.label2.Text = "Số Tài Khoản :";
             // 
@@ -146,73 +152,84 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(150, 343);
+            this.label5.Location = new System.Drawing.Point(100, 235);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(166, 24);
+            this.label5.Size = new System.Drawing.Size(116, 17);
             this.label5.TabIndex = 16;
             this.label5.Text = "Tên Tài Khoản :";
             // 
             // txt_TenTK
             // 
             this.txt_TenTK.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_TenTK.Location = new System.Drawing.Point(322, 340);
+            this.txt_TenTK.Location = new System.Drawing.Point(215, 233);
+            this.txt_TenTK.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_TenTK.Name = "txt_TenTK";
-            this.txt_TenTK.Size = new System.Drawing.Size(380, 32);
+            this.txt_TenTK.Size = new System.Drawing.Size(255, 24);
             this.txt_TenTK.TabIndex = 17;
             // 
             // pictureBox4
             // 
             this.pictureBox4.Image = global::KhachSan.Properties.Resources.Logo_BIDV__1_;
-            this.pictureBox4.Location = new System.Drawing.Point(690, 127);
+            this.pictureBox4.Location = new System.Drawing.Point(460, 71);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(180, 100);
+            this.pictureBox4.Size = new System.Drawing.Size(120, 82);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox4.TabIndex = 21;
             this.pictureBox4.TabStop = false;
             // 
             // pictureBox3
             // 
             this.pictureBox3.Image = global::KhachSan.Properties.Resources.agribank_logo__1_;
-            this.pictureBox3.Location = new System.Drawing.Point(489, 116);
+            this.pictureBox3.Location = new System.Drawing.Point(326, 71);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(180, 100);
+            this.pictureBox3.Size = new System.Drawing.Size(120, 82);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 20;
             this.pictureBox3.TabStop = false;
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = global::KhachSan.Properties.Resources.logo_vietcombank__1_;
-            this.pictureBox2.Location = new System.Drawing.Point(283, 128);
+            this.pictureBox2.Location = new System.Drawing.Point(189, 71);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(180, 100);
+            this.pictureBox2.Size = new System.Drawing.Size(120, 82);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 19;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::KhachSan.Properties.Resources.vietinbank_logo__1_;
-            this.pictureBox1.Location = new System.Drawing.Point(84, 85);
+            this.pictureBox1.Location = new System.Drawing.Point(56, 71);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(180, 120);
+            this.pictureBox1.Size = new System.Drawing.Size(120, 82);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 18;
             this.pictureBox1.TabStop = false;
             // 
             // cbb_NganHang
             // 
             this.cbb_NganHang.EditValue = "";
-            this.cbb_NganHang.Location = new System.Drawing.Point(322, 238);
+            this.cbb_NganHang.Location = new System.Drawing.Point(215, 163);
+            this.cbb_NganHang.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cbb_NganHang.Name = "cbb_NganHang";
             this.cbb_NganHang.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbb_NganHang.Properties.Appearance.Options.UseFont = true;
             this.cbb_NganHang.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-            this.cbb_NganHang.Size = new System.Drawing.Size(381, 30);
+            this.cbb_NganHang.Size = new System.Drawing.Size(254, 22);
             this.cbb_NganHang.TabIndex = 12;
             // 
             // frmThongTinNganHang
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(943, 466);
+            this.ClientSize = new System.Drawing.Size(629, 319);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
@@ -228,6 +245,7 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.toolStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmThongTinNganHang";
